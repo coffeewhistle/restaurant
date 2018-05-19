8259456078633cac5a6646f195e7bd6d91a6a44c
Collier <cole.hansen@gmail.com>
2018-05-19 12:00:03 -0700

    Break out pages to add.html and view.html

--- server.js
+++ server.js
@@ -14,7 +14,17 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
 
+app.get('/view', function(req, res) {
+    
+});
+
+app.get('/add', function(req, res) {
+
+});
 
 // Starts the server to begin listening
 // =============================================================
